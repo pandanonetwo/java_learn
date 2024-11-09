7666f21e69e57691b4263db504fd648f639c71a1
xiashun <1615433530@qq.com>
2024-11-09 17:23:02 +0800

    开始学习java

--- src/test.java
+++ src/test.java
@@ -1,7 +1,13 @@
 public class test {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main (String[] args) {
+        cat cat1 = new cat();
     }
 }
+
 // 新建一个注释测试git功能是否实现呢
 // 测试github是否能够顺利推送呢
+class cat{
+    String name;
+    int age;
+    String color;
+}
